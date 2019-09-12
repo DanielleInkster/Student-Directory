0f6d179b408af396ae0ca4e65f67e8f7b4163061
DanielleInkster <danielle.inkster@gmail.com>
2019-09-11 21:46:55 -0400

    PrintNameLength

--- directory.rb
+++ directory.rb
@@ -22,7 +22,9 @@ end
 
 def print(students)
 students.each_with_index do |student, index|
+  if student[:name].length > 11
   puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+end
 end
 end
 
