7769e0bd40c12457a2a5b704a1a7daabb59972e4
DanielleInkster <danielle.inkster@gmail.com>
2019-09-15 12:53:07 +0100

    Refactor

--- directory.rb
+++ directory.rb
@@ -1,9 +1,8 @@
+@students = []
 
 def input_students
   puts "Please enter the name of the student".center(75)
   puts "To finish, just hit return twice".center(75)
-
-  students = []
 
   name = gets.chop
 
@@ -14,43 +13,63 @@ def input_students
       cohort = "TBD"
     end
 
-    students << {name: name, cohort: cohort}
-    if students.count == 1
-    puts "Now we have #{students.count} student".center(75)
+    @students << {name: name, cohort: cohort}
+    if @students.count == 1
+    puts "Now we have #{@students.count} student".center(75)
   else
-    puts "Now we have #{students.count} students".center(75)
+    puts "Now we have #{@students.count} students".center(75)
   end
   puts "Please enter the name of the next student".center(75)
     name = gets.chop
   end
-  students.sort_by{|student| student[:cohort]}
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 
-def print_header(students)
-  if students.length > 0
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+    case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again?"
+end
+end
+
+def print_header
   puts "The students of Villains Academy".center(75)
   puts '-------------'.center(75)
 end
-end
 
-def print(students)
-  if students.length > 0
-students.each_with_index do |student, index|
-  puts "#{index+1}. #{student[:name].capitalize} (#{student[:cohort].capitalize} cohort).".center(75)
-end
+def print_student_list
+    @students.each do |student|
+    puts "#{student[:name].capitalize} (#{student[:cohort].capitalize} cohort).".center(75)
+  end
 else puts "No students".center(75)
 end
+
+def print_footer
+puts "Overall, we have #{@students.count} great students".center(75)
 end
 
-
-def print_footer(students)
-  if students.length > 0
-puts "Overall, we have #{students.count} great students".center(75)
-end
-end
-
-students = input_students
-print_header(students)
-print(students)
-print_footer(students)
+interactive_menu
